ae0e95dec60539db8f8b10a5d500669a288db4fd
Duncan Marsh <dwolfm@gmail.com>
2016-03-01 17:34:40 -0800

    Update greet.js

--- week-1/mar-1/lecture-code/require-cache/lib/greet.js
+++ week-1/mar-1/lecture-code/require-cache/lib/greet.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// un-comment one of the outsideName variables then run cash-demo.js
+// then switch which one is uncovered and re run cash-demo.js
+// do you notice the difference?
+
 //var outsideName = null;
 var greet = module.exports = function(name){
   //var outsideName = null;
