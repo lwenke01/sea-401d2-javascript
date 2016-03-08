b4245be7cf4792aca2c38bb9779d6ef4f2d87c67
Charles D. Crawford III <charliec364@gmail.com>
2016-03-08 02:44:24 -0500

    add week 2 day 1

---
+++ week-2/1-tcp/inclass-code/tcp.js
@@ -0,0 +1,15 @@
+var net = require('net')
+
+var server = net.createServer(function(connection) {
+  connection.on('data', function(data) {
+    console.log(data.toString('utf-8'))
+  })
+
+  connection.on('end', function() {
+    console.log('bye!')
+  })
+})
+
+server.listen(3000, function() {
+  console.log('server started')
+})
