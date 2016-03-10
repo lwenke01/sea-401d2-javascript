f98a91fa31481be4e9d087384020d46beffd6569
Jason Studstill <jason@jasonstudstill.com>
2016-03-09 17:34:47 -0800

    march 9 final working routes

---
+++ week-2/mar-9-vanilla-routes/inclass-code/ghetto-routing/server.js
@@ -0,0 +1,11 @@
+var http = require('http')
+
+http.createServer(function(req, res) {
+  if (req.url = '/names'&& req.method == 'get') {
+    // do stuff
+  })
+  if (req.url = '/names'&& req.method == 'post') {
+    // do stuff
+  })
+
+})
